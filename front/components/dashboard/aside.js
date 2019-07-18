5ffffc04db792defb8d24c7e1201fc5e6be97a9e
Jongkeun Choi <ynifamily3@gmail.com>
2019-07-18 16:31:37 +0900

    workflow walkthrough

--- front/components/dashboard/aside.js
+++ front/components/dashboard/aside.js
@@ -1,6 +1,22 @@
 import "../../css/dashboard/aside.css";
+import { Button, Icon } from "semantic-ui-react";
 export default ({ children }) => (
   <aside className="dashboardAside">
-    <div>대시보드의 왼쪽 담당</div>
+    <div className="">
+      <Button icon labelPosition="left" basic color="blue">
+        <Icon name="plus" />
+        새로 만들기
+      </Button>
+    </div>
+    <div className="">
+      <ul className="">
+        <li>내 문제함</li>
+        <li>공유 문제함</li>
+        <hr />
+        <li>휴지통</li>
+        <hr />
+        <li>저장용량</li>
+      </ul>
+    </div>
   </aside>
 );
--- front/next.config.js
+++ front/next.config.js
@@ -2,27 +2,26 @@ const withCSS = require("@zeit/next-css");
 const withProgressBar = require("next-progressbar");
 const WebpackBar = require("webpackbar");
 
-module.exports = withProgressBar(
-  withCSS({
-    progressBar: {
-      profile: true
-    },
-    webpack(config, options) {
-      config.module.rules.push({
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 100000
-          }
+module.exports = withCSS({ //withProgressBar(
+  progressBar: {
+    profile: true
+  },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000
         }
-      });
-      config.plugins = config.plugins || [];
-      config.plugins.push(new WebpackBar());
-      return config;
-    }
-  })
-);
+      }
+    });
+    config.plugins = config.plugins || [];
+    // config.plugins.push(new WebpackBar());
+    return config;
+  }
+});
+//);
 /*
 react semantic ui 문제로 https://github.com/zeit/next.js/issues/3825 참고하여 해결함.
 그런데 메모리 누수가 발생함.. => CDN 불러오는 걸로 일단 해결(?) 했음
