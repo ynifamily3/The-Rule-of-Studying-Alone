cf7d5643a5a57cff5a6039db965927d191d70eb8
Miel(최종근) <ynifamily3@gmail.com>
2019-09-17 20:39:59 +0900

    디자인 적용

--- sgma_service/front/pages/index.js
+++ sgma_service/front/pages/index.js
@@ -1,108 +1,230 @@
 import React, { useState, useEffect } from "react";
 import Page from "../layouts/main";
 import Gnb from "../layouts/gnb";
-import { Segment } from "semantic-ui-react";
+import { Loader, Dimmer, Segment } from "semantic-ui-react";
 import { useSelector, useDispatch } from "react-redux";
-import { LOG_IN_FAILURE, LOG_IN_SUCCESS } from "../reducers/userinfo";
-import CustomModal from "../components/modal/custommodal";
+import { LOG_IN_FAILURE, LOG_IN_SUCCESS, LOG_OUT } from "../reducers/userinfo";
+// import CustomModal from "../components/modal/custommodal";
 import axios from "axios";
 
-import { ADD_SUBJECT } from "../reducers/subjects";
+// css-loader works
+import "../css/main/common.css";
+import { Router, useRouter } from "next/router";
 
 const IndexPage = ctx => {
   // ajax로 로그인 상태 검사하여 직접 LOG_IN_SUCCESS를 dispatch
-  // 그 전 까진 앱을 모달로 얼려놓는다.
+  const router = useRouter();
   const dispatch = useDispatch();
-  // manage modal state
-  const [modalIsOpen, setModalIsOpen] = useState(false); // 모달의 기볹값은 false (ssr환경에서 true로 하면 이상한 종속성 에러가 표출된다.)
-  const onChangeModalIsOpen = e => {
-    setModalIsOpen(false);
-  };
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [user, setUser] = useState({ isLogin: false });
 
   useEffect(() => {
-    /*
-    // *** 테스트 : dispatch
-    dispatch({
-      type: ADD_SUBJECT,
-      data: {
-        subject_name: "리시프 좋아.."
-      }
-    });
-
-    // end of test
-    */
-    // *** 혹은 getInitialProps 후킹 걸어놓고, 서버 사이드 / 클라이언트 사이드 처리를 이원화하고,
-    // 서버의 경우 api 서버와의 통신이 완료된 경우 뿌려주고 ,클라는 모달로 한다음 클라에서 api를 로딩하고 결정
-    setModalIsOpen(true); // BLOCK 걸어놓고 state에 대한 서버 검증 (갱신)
     axios(`${process.env.BACKEND_SERVICE_DOMAIN}/api/userinfo`, {
       withCredentials: true
-    }).then(resp => {
-      setModalIsOpen(false);
-      const loginTest = resp.data;
-      if (!loginTest.isLogin) {
-        dispatch({ type: LOG_IN_FAILURE });
-      } else {
-        dispatch({
-          type: LOG_IN_SUCCESS,
-          data: {
-            ...loginTest
-          }
-        });
-      }
-      return loginTest; // 안 해도 되나?
+    })
+      .then(resp => {
+        const loginTest = resp.data;
+        setIsLoaded(true);
+        if (!loginTest.isLogin) {
+          dispatch({ type: LOG_IN_FAILURE });
+        } else {
+          dispatch({
+            type: LOG_IN_SUCCESS,
+            data: {
+              ...loginTest
+            }
+          });
+          setUser(loginTest);
+        }
+        return loginTest;
+      })
+      .catch(e => {
+        alert("서버 통신 중 오류 발생!!\n페이지를 새로 고침 해 주세요");
+      });
+  }, []);
+
+  const loginClick = e => {
+    router.push(`/login`);
+  };
+
+  const logoutClick = e => {
+    dispatch({
+      type: LOG_OUT
     });
-  }, []); // componentDidMount와 유사함., deps를 주면 ([]라도,) 한 번만 실행
+    setUser({ isLogin: false });
+  };
+
+  const findSubjectClick = e => {
+    router.push(`/subjects`);
+  };
 
   return (
     <Page>
-      <CustomModal
-        open={modalIsOpen}
-        closeOnEscape={false}
-        // esc로 탈출 불가
-        closeOnDimmerClick={false}
-        // 외부클릭으로 탈출 불가
-        onClose={onChangeModalIsOpen}
-        message={"서버와 통신 중입니다..."}
-        dimmer={"inverted"}
-      />
-      <Gnb />
+      {isLoaded ? (
+        <div>
+          <style jsx global>
+            {`
+              .header {
+                height: 100px;
+                width: 100%;
+                display: flex;
+                flex-direction: row;
+                justify-content: space-around;
+              }
 
-      <Segment
-        style={{
-          width: "50%",
-          minWidth: "380px",
-          margin: "auto 94.5px",
-          marginTop: "30px"
-        }}
-      >
-        <h2>혼공의 정석</h2>
-        <h3>소개</h3>
-        {`아직도 백지에 모든 걸 쓰며
-      불필요한 정보까지 외우시나요?
-      복학생이라 같이 교양 들을 친구가 없다구요?
-      교수님이 말장난을 너무 잘하셔서 스탠딩 코미디도 나가신다구요? 이제
-      걱정하지 마세요. 혼공의 정석이 여러분의 암기를 책임집니다!
-      혼공의 정석은 교양 시험, 공무원 시험 등 단순 암기가 필요할 때
-      공부를 도와주고 암기 상태를 확인할 수 있는 웹 어플리케이션입니다. 세상의
-      모든 단순암기시험이 사라지는 그날까지 여러분을 응원합니다.`}
-        <h3>기능</h3>
-        <ul
-          style={{
-            textIndent: "1.5em",
-            listStyle: "none"
-          }}
-        >
-          <li>주제를 만들고 지식 입력하기</li>
-          <li>내가 입력한 지식 공유하기</li>
-          <li>암기를 할 수 있도록 연습문제 풀기</li>
-          <li>암기 상태를 점검할 수 있는 모의고사</li>
-          <li>어디를 덜 외웠는지 확인할 수 있는 암기점검 기능</li>
-          <li>
-            (프리미엄) 교육 서비스 업체와 제휴하여, 프리미엄 이용권 사용자를
-            위한 독점 문제 제공
-          </li>
-        </ul>
-      </Segment>
+              .left {
+                flex-grow: 1;
+                display: flex;
+                justify-content: flex-start;
+                margin-left: 50px;
+              }
+
+              .right {
+                flex-grow: 1;
+                display: flex;
+                justify-content: flex-end;
+                margin-right: 50px;
+              }
+
+              /* MAIN IMAGE */
+              .main-img {
+                overflow: hidden;
+                height: 300px;
+              }
+
+              .on-img-text {
+                position: absolute;
+                font-size: 4vw;
+                color: #ffffff;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+              }
+
+              /* INTRO TEXT */
+              .intro {
+                display: flex;
+                justify-content: center;
+                height: 200px;
+              }
+
+              .intro img {
+                align-self: center;
+                padding-left: 50px;
+                padding-right: 50px;
+              }
+
+              .intro-text {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                width: 33%;
+                max-width: 300px;
+              }
+
+              /* COUNTERS */
+              .counters {
+                width: 100%;
+                height: 100px;
+                display: flex;
+                flex-direction: ltr;
+                justify-content: space-evenly;
+              }
+
+              .counter-elem {
+                border: 1px double white;
+                padding: 20px;
+                align-self: center;
+              }
+            `}
+          </style>
+          <div className="header hongong_solid">
+            <span className="left">
+              <img
+                src="/static/img/logo-small.png"
+                style={{ width: "177px", height: "100px" }}
+              />
+              {user.isLogin && (
+                <button className="hongong-button" onClick={findSubjectClick}>
+                  과목찾기
+                </button>
+              )}
+              {user.isLogin && (
+                <button className="hongong-button">과목만들기</button>
+              )}
+            </span>
+            <span className="right">
+              {!user.isLogin && (
+                <button className="hongong-button">회원가입</button>
+              )}
+              {!user.isLogin && (
+                <button className="hongong-button" onClick={loginClick}>
+                  로그인
+                </button>
+              )}
+              {user.isLogin && (
+                <button className="hongong-button" onClick={logoutClick}>
+                  로그아웃 ({user.user.nickname})
+                </button>
+              )}
+            </span>
+          </div>
+          <div className="main-img" style={{ position: "relative" }}>
+            <img src="/static/img/main0-dark.jpg" style={{ width: "100%" }} />
+            <span className="on-img-text">이제 암기 걱정은 그만!</span>
+          </div>
+
+          <div className="intro">
+            <img src="/static/img/arrow-right.jpg" />
+            <span className="intro-text">
+              <span style={{ alignSelf: "flex-start" }}>
+                <font size="6em">혼</font>
+                <font size="4em">공의 </font>
+                <font size="6em">정석</font>
+                <font size="4em">은</font>
+              </span>
+
+              <span style={{ alignSelf: "center" }}>
+                <font size="6em">혼</font>
+                <font size="4em">자서도 </font>
+                <font size="6em">공</font>
+                <font size="4em">부할 수 있는</font>
+              </span>
+
+              <span style={{ alignSelf: "flex-end" }}>
+                <font size="4em">공부 도우미 서비스입니다.</font>
+              </span>
+            </span>
+            <img src="/static/img/arrow-left.jpg" />
+          </div>
+          <div className="counters hongong_solid">
+            <span className="counter-elem">
+              <font id="counter-subjects" size="5em">
+                1024
+              </font>{" "}
+              과목
+            </span>
+            <span className="counter-elem">
+              <font id="counter-attrs" size="5em">
+                2147483647
+              </font>{" "}
+              지식
+            </span>
+            <span className="counter-elem">
+              <font id="counter-groups" size="5em">
+                644
+              </font>{" "}
+              그룹
+            </span>
+          </div>
+        </div>
+      ) : (
+        <div style={{ height: "100vh" }}>
+          <Dimmer active inverted>
+            <Loader inverted>Connecting ...</Loader>
+          </Dimmer>
+        </div>
+      )}
     </Page>
   );
 };
--- sgma_service/front/pages/subjects.js
+++ sgma_service/front/pages/subjects.js
@@ -7,6 +7,9 @@ import { ADD_SUBJECT, FETCH_SUBJECT } from "../reducers/subjects";
 import SelectionComponent from "../components/subjects/selection";
 import axios from "axios";
 
+// css-loader works
+import "../css/main/common.css";
+
 // subjects들을 선택하는 페이지이다.
 
 const SubjectsPage = () => {
@@ -72,20 +75,102 @@ const SubjectsPage = () => {
         message={"서버와 통신 중입니다..."}
         dimmer={"inverted"}
       />
-      <div>무엇을 공부하시겠습니까?</div>
-      <Form>
-        <legend>
-          로그인 정보 :
-          {userName === null ? "Loading" : !!userName ? userName : "손님"}
-        </legend>
-        <Input type="text" onChange={changeAction} value={subject} />
-        <br />
-        <br />
-        <Button basic color="blue" onClick={takeAction}>
-          과목 생성하기
-        </Button>
-      </Form>
-      <SelectionComponent isLogin={isLogin} />
+      <style jsx global>
+      {`
+      body {
+        line-height: initial;
+      }
+      .header {
+			height: 100px;
+			width: 100%;
+			display: flex;
+			flex-direction: row;
+			justify-content: space-around;
+		}
+
+		.left {
+			flex-grow: 1;
+			display: flex;
+			justify-content: flex-start;
+			margin-left: 50px;
+		}
+
+		.right {
+			flex-grow: 1;
+			display: flex;
+			justify-content: flex-end;
+			margin-right: 50px;
+		}
+
+		.editor-bound {
+			display: flex;
+			flex-direction: column;
+			justify-content: flex-start;
+			border: 1px solid gray;
+			padding: 15px;
+		}
+
+		.editor-tags {
+			display: flex;
+			flex-direction: row;
+			justify-content: flex-start;
+			align-content: center;
+			margin-top: 10px;
+			margin-bottom: 10px;
+		}
+
+		.editor-tag-elem {
+			background: #feda5f;
+			border: none;
+			border-radius: 0.3em;
+			padding-top: 0.2em;
+			padding-left: 0.5em;
+			padding-right: 0.5em;
+			margin-right: 0.8em;
+			height: 1.2em;
+			font-size: 0.8em;
+			text-align: bottom;
+			display: flex;
+		}`}
+      </style>
+      <div className='header hongong_solid'>
+		<span className='left'>
+			<img src='/static/img/logo-small.png' style={{width: '177px', height: '100px'}} />
+			<button className='hongong-button'>과목찾기</button>
+			<button className='hongong-button'>과목만들기</button>
+		</span>
+		<span className='right'>
+			<button className='hongong-button'>회원가입</button>
+			<button className='hongong-button'>로그인</button>
+		</span>
+	</div>
+
+	<div style={{margin: '25px'}}>
+		<h1>과목 만들기</h1>
+		<div className='editor-bound'>
+			<div>
+			<font size='5em'><a>과목명 </a><input type='textarea' className='hongong-textarea' id='subject-name' /></font></div>
+			<div className='editor-tags'>
+				<span className='editor-tag-elem'>#기출</span>
+				<span className='editor-tag-elem'>#리시프</span>
+			</div>
+			<div>
+				<a>분류 </a><input type='textarea' className='hongong-textarea' id='category' /><br/>
+				<a>소속 </a><input type='textarea' className='hongong-textarea' id='foundation' /><br/>
+				<a>내용 </a><input type='textarea' className='hongong-textarea' id='description' />
+			</div>
+			<div style={{display: 'flex', paddingTop: '10px'}}>
+				<font size='1em'>
+				<button className='hongong-button' id='confirm'>확인</button>
+				<button className='hongong-button' id='cancle'>취소</button>
+				</font>
+			</div>
+		</div>
+	</div>
+
+
+
+
     </Page>
   );
 };
